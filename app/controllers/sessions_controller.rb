d0ba8dabf882e1008947863f0d9c8ff1d3dcf789
Ramesh K <rameshkror@gmail.com>
2013-08-29 17:36:46 +0530

    New Files

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+  session[:username] = params[:username]
+  render :text => "Welcome #{session[:username]}!"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoNet::Application.config.secret_token = 'f2b3196a4294252c7337bf9be005a568ba2a5d6dbeb365d8f30a22878abb4aa9d2178075f82443509e5b96280a0cefeec21f912ff3b74e3cf1d0ee5473b1dbdd'
